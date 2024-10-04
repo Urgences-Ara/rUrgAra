b2cabf8cc8efb4921c250bacea6698e108191592
Clément Claustre <clement.claustre@urgences-ara.fr>
2024-10-04 16:04:02 +0200

    Correction bug. La fonction clean finess ne prend pas en compte les NA

--- R/clean_FINESS.R
+++ R/clean_FINESS.R
@@ -15,7 +15,7 @@
 #' clean_FINESS(c("10784320", "380000174"))
 clean_FINESS <- function(char){
   if(!is.character(char)){stop("char must be a character vector.")}
-  if(any(!nchar(char) %in% c(8, 9))){stop("FINESS should be 9 or 8 characters. Some occurences are different.")}
+  if(any(!nchar(char) %in% c(8, 9) & !is.na(char))){stop("FINESS should be 9 or 8 characters. Some occurences are different.")}
   char = if_else(nchar(char) == 8, paste0("0", char), as.character(char))
   return(char)
 }
