acff79cfc89b7d195adfb3fc75910e56cf2533b5
Clément Claustre <clement.claustre@urgences-ara.fr>
2024-10-25 15:22:58 +0200

    Opitimisation clean_commune par vectorisation du str_remove_all

--- R/clean_commune.R
+++ R/clean_commune.R
@@ -17,25 +17,31 @@
 #' clean_commune(c("La tronche", "st-etienne", "Saint     Étienne", "Saint-étienne"))
 clean_commune <- function(commune, CP = NULL, info = TRUE){
   commune_propre = commune
+
+
   #Recodage général
   commune_propre = toupper(trimws(commune_propre))
-  commune_propre = str_replace_all(commune_propre, "(?<![A-Z])ST[- ]", "SAINT ")
-  commune_propre = str_replace_all(commune_propre, "(?<![A-Z])STE[- ]", "SAINTE ")
-  commune_propre = str_replace_all(commune_propre, "(?<![A-Z])MT[- ]", "MONT ")
-  commune_propre = str_replace_all(commune_propre, "\\/", " SUR ")
-  commune_propre = str_replace_all(commune_propre, "S\\/", " SUR ")
-  commune_propre = str_replace_all(commune_propre, "[\\\'\\-\\`\\.\\\u2019]", " ")
+  commune_propre = str_replace_all(commune_propre,
+                                   c("(?<![A-Z])ST[- ]" = "SAINT ",
+                                     "(?<![A-Z])STE[- ]" = "SAINTE ",
+                                     "(?<![A-Z])MT[- ]" = "MONT ",
+                                     "\\/" = " SUR ",
+                                     "S\\/" = " SUR ",
+                                     "[\\\'\\-\\`\\.\\\u2019]" = " ",
+                                     "[\u00c8\u00c9\u00ca\u00cb]" = "E",
+                                     "[\u00d4\u00d6]" = "O",
+                                     "[\u00c4\u00c2\u00c0]" = "A",
+                                     "[\u00ce\u00cf]" = "I",
+                                     "[\u00db\u00dc]" = "U",
+                                     "[\u00c7]" = "C",
+                                     "LYON0" = "LYON 0")
+
+  )
+
   commune_propre = str_remove_all(commune_propre, "\\(.*\\)")
   commune_propre = str_replace_all(commune_propre, " {2,}", " ")
   commune_propre = str_remove_all(commune_propre, "[0-9]{3,}")
   commune_propre = str_remove_all(commune_propre, " CEDEX(?![A-Z])")
-  commune_propre = str_replace_all(commune_propre,"[\u00c8\u00c9\u00ca\u00cb]", "E")
-  commune_propre = str_replace_all(commune_propre,"[\u00d4\u00d6]", "O")
-  commune_propre = str_replace_all(commune_propre,"[\u00c4\u00c2\u00c0]", "A")
-  commune_propre = str_replace_all(commune_propre,"[\u00ce\u00cf]", "I")
-  commune_propre = str_replace_all(commune_propre,"[\u00db\u00dc]", "U")
-  commune_propre = str_replace_all(commune_propre,"[\u00c7]", "C")
-  commune_propre = str_replace_all(commune_propre, "LYON0", "LYON 0")
   commune_propre = trimws(commune_propre)
 
   #Recodage à partir du CP
